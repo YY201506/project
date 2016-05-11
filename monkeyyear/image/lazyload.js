b323bf93f78fcf5651bc7a05cb2aa460cf458901
lin <1076633936@qq.com>
2016-05-11 17:10:38 +0800

    iscroll 滑动定位

---
+++ monkeyyear/image/lazyload.js
@@ -0,0 +1,23 @@
+function lazyload(window,document,undefined){
+
+    var obj={
+        container:window,
+        element:document.getElementsByTagName("img"),
+        setting:{
+            threshold: 0,
+            failure_limit: 0,
+            event: "scroll",
+            effect: "show",
+            container: window,
+            data_attribute: "original",
+            skip_invisible: false,
+            appear: null,
+            load: null,
+            placeholder: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+        }
+    };
+    function update(){}
+
+
+
+}
